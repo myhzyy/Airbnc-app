0c38d37e021a40b88d8cd0d4d5e306ca75c738db
myhzy <notmyhzy@hotmail.com>
2025-06-26 08:28:10 +0100

    add like counter

--- src/components/PropertyCard.jsx
+++ src/components/PropertyCard.jsx
@@ -5,13 +5,21 @@ export default function PropertyCard({ property }) {
 
   return (
     <div className="propertyCard-square">
-      <img className="propertyCard-image" src={property.image} alt="" />
+      <div className="propertyCard-image-wrapper">
+        <img className="propertyCard-image" src={property.image} alt="" />
+        <span className="property-info-likes">
+          ❤️{property.favourite_count}
+        </span>
+      </div>
 
       <div className="propertyInfo">
         <h2 className="property-title">{property.property_name}</h2>
         <p className="property-info">{property.location}</p>
         <p className="property-info">£{property.price_per_night}</p>
-        <p className="property-info">{property.host}</p>
+
+        <div className="property-info-favourite-container">
+          <p className="property-info">{property.host}</p>
+        </div>
       </div>
     </div>
   );
