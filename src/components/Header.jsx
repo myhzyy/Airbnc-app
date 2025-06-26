68a20ec5b9393710312e3e7bdc4e0fcdb7ed4333
myhzy <notmyhzy@hotmail.com>
2025-06-26 12:39:03 +0100

    front page layout draft complete

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,9 @@
 import "./Header.css";
 import headerLogo from "../assets/images.png";
+import signInImage from "../assets/defaultSignInImage.png";
+import cabinImage from "../assets/header-cabin-image.png";
+import searchbarIcon from "../assets/search-bar-icon2.png";
+
 import { useEffect } from "react";
 
 export default function Header() {
@@ -7,14 +11,33 @@ export default function Header() {
     <div className="header">
       <div className="header-bar">
         <img className="header-logo" src={headerLogo} alt="image" />
-        <div className="checkIn-checkOut">
-          <p>check in</p>
-          <p>check out</p>
+
+        <div className="stays-container">
+          <img
+            className="stays-image"
+            src={cabinImage}
+            alt="header cabin image"
+          />
+          <p>Stays</p>
         </div>
 
-        <input className="search-bar" type="text" placeholder="search" />
-        <input className="search-bar" type="text" placeholder="search button" />
-        <p>side button</p>
+        <div className="header-searchbar">
+          <img
+            className="searchbar-icon"
+            src={searchbarIcon}
+            alt="searchbar icon"
+          />
+          <input
+            className="header-search-bar"
+            type="text"
+            placeholder="search for your next adventure..."
+          />
+        </div>
+
+        <div className="logIn">
+          <p>Log in</p>
+          <img className="signIn-image" src={signInImage} alt="" />
+        </div>
       </div>
     </div>
   );
